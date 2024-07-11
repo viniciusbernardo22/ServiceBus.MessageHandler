8879cc9b01eb2308aca21ab3f086630d6ff59c34
viniciusbernardo22 <vini383@gmail.com>
2024-07-11 15:14:43 -0300

    feat: implemented message processing

--- ServiceBus.MessageHandler.Receiver/Program.cs
+++ ServiceBus.MessageHandler.Receiver/Program.cs
@@ -1,7 +1,46 @@
-﻿using ServiceBus.MessageHander.Shared.Instances;
+﻿using Azure.Messaging.ServiceBus;
+using ServiceBus.MessageHander.Shared.Instances;
 
+ServiceBusProcessor processor = default!;
 
+var serviceBusInstance = new ServiceBusInstance();
 
-var serviceBusClient = new ServiceBusInstance();
+async Task QueueMessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    ServiceBusReceivedMessage message = processMessageEventArgs.Message;
+    string body = message.Body.ToString();
+    Console.WriteLine(body);
+    await processMessageEventArgs.CompleteMessageAsync(message);
+}
 
+ Task MessageErrorHandler(ProcessErrorEventArgs processMessageEventArgs)
+{
+    Console.WriteLine(processMessageEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
 
+var client = serviceBusInstance.GetServiceBusClient();
+processor = client.CreateProcessor(serviceBusInstance.Queue, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += QueueMessageHandler;
+    processor.ProcessErrorAsync += MessageErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end the processing");
+    Console.ReadKey();
+    Console.WriteLine("Stopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving Messages");
+
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error: {e}");
+}
+finally
+{
+   await client.DisposeAsync();
+   await processor.DisposeAsync();
+}
